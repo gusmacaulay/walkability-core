885ffd54b9613d22ada7e9beffe7398a485db94c
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2012-03-19 16:27:45 +1100

    NetworkBuffer now includes walks which include 1 closed loop (invalid Path but valid part of service area).  Start Node is now correctly taken from the snap point.  Moved starting point in test case.

--- walkability/src/main/java/org/mccaughey/connectivity/NetworkBuffer.java
+++ walkability/src/main/java/org/mccaughey/connectivity/NetworkBuffer.java
@@ -46,12 +46,12 @@ import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.graph.build.feature.FeatureGraphGenerator;
 import org.geotools.graph.build.line.LineStringGraphGenerator;
 import org.geotools.graph.path.Path;
-import org.geotools.graph.path.Walk;
 import org.geotools.graph.structure.Edge;
 import org.geotools.graph.structure.Graph;
 import org.geotools.graph.structure.Node;
 import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.filter.Filter;
 import org.opengis.filter.FilterFactory2;
@@ -70,15 +70,13 @@ public final class NetworkBuffer {
     }
 
     public static SimpleFeature createPolygonBuffer(SimpleFeatureSource network, SimpleFeature pointFeature, Double distance) throws IOException {
-
-
         //Build network Graph - within bounds
         Point pointOfInterest = (Point) pointFeature.getDefaultGeometry();
 
         Geometry pointBuffer = pointOfInterest.buffer(distance);
 
         SimpleFeatureCollection networkRegion = featuresInRegion(network, pointBuffer);
-        FeatureGraphGenerator networkGraphGen = buildFeatureNetwork(networkRegion);
+
         // Graph networkGraph = networkGraphGen.getGraph();
         //Snap point to network
         final SpatialIndex index = new STRtree();
@@ -113,6 +111,7 @@ public final class NetworkBuffer {
         // distance plus a little bit
         double minDist = distance + 1.0e-6;
         Coordinate minDistPoint = null;
+        LocationIndexedLine connectedLine = null;
 
         for (LocationIndexedLine line : lines) {
 
@@ -122,9 +121,9 @@ public final class NetworkBuffer {
             if (dist < minDist) {
                 minDist = dist;
                 minDistPoint = point;
+                connectedLine = line;
             }
         }
-
 
         if (minDistPoint == null) {
             // No line close enough to snap the point to
@@ -132,39 +131,116 @@ public final class NetworkBuffer {
 
         } else {
             LOGGER.info("{} - snapped by moving {}\n", pt.toString(), minDist);
-            pointFeature.setDefaultGeometry(pointOfInterest);
-            // System.out.println("Old network features: " + String.valueOf(networkRegion.size()));
-            GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
 
-            Coordinate[] coords =
-                    new Coordinate[]{new Coordinate(pt.x, pt.y), new Coordinate(minDistPoint.x, minDistPoint.y)};
 
-            LineString snapLine = geometryFactory.createLineString(coords);
-            networkGraphGen.add(buildFeatureFromGeometry(networkRegion.features().next(), snapLine));
-            //feature.setDefaultGeometry(pointOfInterest);
-//            System.out.println("Added snapped node");
-//            //Graph networkGraph2 = networkGraphGen.getGraph();
-//            System.out.println("Orginal nodes: " + String.valueOf(networkRegion.size()));
-//            System.out.println("New nodes: " + String.valueOf(networkGraph.getNodes().size()));
+            //networkGraphGen.remove(buildFeatureFromGeometry(networkRegion.getSchema(),connectedLine.extractLine(connectedLine.getStartIndex(), connectedLine.getEndIndex())));
+            Geometry lineA = connectedLine.extractLine(connectedLine.getStartIndex(), connectedLine.project(minDistPoint));
+            Geometry lineB = connectedLine.extractLine(connectedLine.project(minDistPoint), connectedLine.getEndIndex());
+
+//            pointFeature.setDefaultGeometry(pointOfInterest);
+//            GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory(null);
+//            Coordinate[] coords = new Coordinate[]{new Coordinate(pt.x, pt.y), new Coordinate(minDistPoint.x, minDistPoint.y)};
+//            LineString snapLine = geometryFactory.createLineString(coords);
+//           SimpleFeature feature =  buildFeatureFromGeometry(networkRegion.getSchema(), snapLine);
+
+            System.out.println("Orginal nodes: " + String.valueOf(networkRegion.size()));
+            FeatureGraphGenerator networkGraphGen = buildFeatureNetwork(networkRegion);
+            SimpleFeature featureA = buildFeatureFromGeometry(networkRegion.getSchema(), lineA);
+            networkGraphGen.add(featureA);
+            SimpleFeature featureB = buildFeatureFromGeometry(networkRegion.getSchema(), lineB);
+            networkGraphGen.add(featureB);
             Graph graph = networkGraphGen.getGraph();
+            System.out.println("New nodes: " + String.valueOf(graph.getEdges().size()));
+
             Path startPath = new Path();
-            Node startNode = (Node) graph.getNodes().toArray()[0]; //hmm is the snap node the last node?
-            startPath.add(startNode);
+            // Node startNode = (Node) graph.getNodes().toArray()[0]; //hmm is the snap node the last node?
+            //startPath.add(startNode);
+            for (Node node : (Collection<Node>) graph.getNodes()) {
+                if (node.getEdges().size() == 2) {
+                    SimpleFeature edgeFeature1 = (SimpleFeature) (((Edge) node.getEdges().toArray()[0]).getObject());
+                    SimpleFeature edgeFeature2 = (SimpleFeature) (((Edge) node.getEdges().toArray()[1]).getObject());
+
+                    if (edgeFeature1.getID().equals(featureA.getID()) && edgeFeature2.getID().equals(featureB.getID())) {
+                        LOGGER.info("Found start node " + node.getEdges());
+                        startPath.add(node);
+                        break;
+                    }
+                    if (edgeFeature2.getID().equals(featureA.getID()) && edgeFeature1.getID().equals(featureB.getID())) {
+                        LOGGER.info("Found start node " + node.getEdges());
+                        startPath.add(node);
+                        break;
+                    }
+                }
+            }
             List<Path> paths = findPaths(graph, startPath);
             LOGGER.info("Found paths: " + String.valueOf(paths.size()));
             LOGGER.info(pathsToJSON(paths));
             //  LOGGER.info(graphToJSON(graph));
         }
-
-
-        //
-
-        //Follow paths out from point/node of interest, cut lines at distance
-        //        List<Graph> paths = findPaths(startNode,null);
-        //create buffer of subnetwork, polygonize
-
-
         return pointFeature;
+    }
+
+    private static String writeFeatures(SimpleFeatureCollection features) {
+        FeatureJSON fjson = new FeatureJSON();
+        Writer writer = new StringWriter();
+        try {
+            fjson.writeFeatureCollection(features, writer);
+        } catch (Exception e) {
+            return "{}";
+        }
+        return writer.toString();
+    }
+
+    private static List<Path> findPaths(Graph network, Path currentPath) {
+        List<Path> paths = new ArrayList();
+
+        //for each edge connected to current node 
+        for (Node node : (Collection<Node>) network.getNodes()) { //find the current node in the graph (not very efficient)
+            if (node.equals(currentPath.getLast())) {
+                LOGGER.info("Found Current Node in Graph");
+                for (Edge graphEdge : (List<Edge>) node.getEdges()) {
+                    LOGGER.info("Current Node has edges: " + node.getEdges());
+                    if (currentPath.size() < 8) { //if path + edge less than distance
+
+                        Path nextpath = new Path();
+                        nextpath.addEdges(currentPath.getEdges());
+                        if (nextpath.addEdge(graphEdge)) {
+                            LOGGER.info("Appended edge to path: " + nextpath.getEdges());
+                            if (nextpath.isValid()) //check if valid path (no repeated nodes)
+                            {   //append findPaths(path+edge) to list of paths
+                                paths.addAll(findPaths(network, nextpath));
+                                LOGGER.info("Adding edge: " + graphEdge + " ...exploring further");
+                            } else if (nextpath.isClosed()) {  //if the path happens to be a closed path then still add it - don't want to miss out on looped edges - but no need to explore path further
+                                paths.add(nextpath);
+                                LOGGER.info("Adding edge: " + graphEdge + " ...terminating closed walk");
+                            }
+                        } else {
+                            LOGGER.info("Failed to append edge to path");
+                        }
+                    } else {//else chop edge, append (path + chopped edge) to list of paths
+                        Path newpath = new Path();
+                        newpath.addEdges(currentPath.getEdges());
+                        if (newpath.addEdge(graphEdge)) {
+                            LOGGER.info("Appended edge to path: " + newpath.getEdges());
+                            if (newpath.isValid()) {
+                                paths.add(newpath);
+                                LOGGER.info(" ...terminating completed path" + newpath.getEdges());
+                            } else if (newpath.isClosed()) {  //if the path happens to be a closed path then still add it - don't want to miss out on looped edges
+                                paths.add(newpath);
+                                LOGGER.info(" ...terminating completed closed walk: " + newpath.getEdges());
+                            }
+                        } else {
+                            LOGGER.info("Failed to append edge to path");
+                        }
+                    }
+                }
+                //return all paths
+               // return paths;
+            }
+        }
+        paths.add(currentPath);
+        return paths;
+        //return null;
     }
 
     private static String graphToJSON(Graph graph) {
@@ -174,20 +250,6 @@ public final class NetworkBuffer {
         for (Edge edge : (Collection<Edge>) graph.getEdges()) {
             features.add(((SimpleFeature) edge.getObject()));
         }
-        return (writeFeatures(DataUtilities.collection(features)));
-    }
-
-    private static String walksToJSON(List<Walk> walks) {
-        String json = "";
-        List<SimpleFeature> features = new ArrayList();
-        for (Walk walk : walks) {
-            LOGGER.info("Walk: " + String.valueOf(walk.getEdges()));
-            for (Edge edge : (List<Edge>) walk.getEdges()) {
-                features.add(((SimpleFeature) edge.getObject()));
-            }
-            //  LOGGER.info(writeFeatures(DataUtilities.collection(features)));
-        }
-
         return (writeFeatures(DataUtilities.collection(features)));
     }
 
@@ -205,155 +267,11 @@ public final class NetworkBuffer {
         return (writeFeatures(DataUtilities.collection(features)));
     }
 
-    private static String writeFeatures(SimpleFeatureCollection features) {
-        FeatureJSON fjson = new FeatureJSON();
-        Writer writer = new StringWriter();
-        try {
-            fjson.writeFeatureCollection(features, writer);
-        } catch (Exception e) {
-            return "{}";
-        }
-        return writer.toString();
-
-    }
-
-     private static List<Path> findPaths(Graph network, Path currentPath) {
-        List<Path> paths = new ArrayList();
-
-        //for each edge connected to current node 
-        for (Node node : (Collection<Node>) network.getNodes()) { //find the current node in the graph (not very efficient)
-            if (node.equals(currentPath.getLast())) {
-//                LOGGER.info("Found currentNode in network graph: " + currentPath.getLast());
-                for (Edge graphEdge : (List<Edge>) node.getEdges()) {
-//                    LOGGER.info("Graph Node Edges: " + node.getEdges());
-//                    LOGGER.info("Path Node Edges: " + currentPath.getEdges());
-//                    LOGGER.info("Found new connected edge: " + graphEdge);
-                    if (currentPath.size() < 8) { //if path + edge less than distance
-
-                        //append findPaths(path+edge) to list of paths
-                        //Path nextpath = (Path)currentpath.clone();
-                        //   Path nextpath = addEdge(graphEdge, currentpath);
-                        Path nextpath = new Path();
-                        nextpath.addEdges(currentPath.getEdges());
-                        if (nextpath.addEdge(graphEdge)) {
-//                            LOGGER.info("Exploring path beyond new edge: " + nextpath.getEdges());
-                            if (nextpath.isValid())
-                                paths.addAll(findPaths(network, nextpath));
-                        }
-                    } else {//else chop edge, append (path + chopped edge) to list of paths
-//                        LOGGER.info("Adding new (chopped) edge: " + graphEdge + " to current Path: " + currentPath.getEdges());
-                        //  Path newpath = addEdge(graphEdge, currentpath);
-                        Path newpath = new Path();
-                        newpath.addEdges(currentPath.getEdges());
-                        if (newpath.addEdge(graphEdge)) {
-//                            LOGGER.info("New Path: " + newpath.getEdges());
-                            if (newpath.isValid())
-                                paths.add(newpath);
-                        }
-                    }
-                }
-                //return all paths
-                return paths;
-            }
-        }
-        LOGGER.info("FAIL!");
-        return null;
-    }
-    
-    private static List<Walk> findWalks(Graph network, Walk currentWalk) {
-        List<Walk> walks = new ArrayList();
-
-        //for each edge connected to current node 
-        for (Node node : (Collection<Node>) network.getNodes()) { //find the current node in the graph (not very efficient)
-            if (node.equals(currentWalk.getLast())) {
-                LOGGER.info("Found currentNode in network graph: " + currentWalk.getLast());
-                for (Edge graphEdge : (List<Edge>) node.getEdges()) {
-                    LOGGER.info("Graph Node Edges: " + node.getEdges());
-                    LOGGER.info("Path Node Edges: " + currentWalk.getEdges());
-                    LOGGER.info("Found new connected edge: " + graphEdge);
-                    if (currentWalk.size() < 8) { //if path + edge less than distance
-
-                        //append findPaths(path+edge) to list of paths
-                        //Walk nextWalk = (Walk)currentWalk.clone();
-                        //   Walk nextWalk = addEdge(graphEdge, currentWalk);
-                        Walk nextWalk = new Walk();
-                        nextWalk.addEdges(currentWalk.getEdges());
-                        if (nextWalk.addEdge(graphEdge)) {
-                            LOGGER.info("Exploring path beyond new edge: " + nextWalk.getEdges());
-                            walks.addAll(findWalks(network, nextWalk));
-                        }
-                    } else {//else chop edge, append (path + chopped edge) to list of paths
-                        LOGGER.info("Adding new (chopped) edge: " + graphEdge + " to current walk: " + currentWalk.getEdges());
-                        //  Walk newWalk = addEdge(graphEdge, currentWalk);
-                        Walk newWalk = new Walk();
-                        newWalk.addEdges(currentWalk.getEdges());
-                        if (newWalk.addEdge(graphEdge)) {
-                            LOGGER.info("New Walk: " + newWalk.getEdges());
-                            walks.add(newWalk);
-                        }
-                    }
-                }
-
-                //return all paths
-                return walks;
-            }
-        }
-        LOGGER.info("FAIL!");
-        return null;
-    }
-
-    static public Walk addEdge(Edge e, Walk walk) {
-        //append edge to end of path, path must be empty, or last node in path
-        // must be a node of the edge
-
-//  	//save current edge list
-//  	List edges = walk.getEdges();
-        Walk newWalk = new Walk();
-        newWalk.addEdges(walk.getEdges());
-
-        if (newWalk.isEmpty()) {
-            //add both nodes
-            newWalk.add(e.getNodeA());
-            newWalk.add(e.getNodeB());
-        } else {
-            //walk is not empty, check to see if the last node is related to the edge
-            Node last = newWalk.getLast();
-            LOGGER.info("Node Last: " + last + " Node A: " + e.getNodeA() + " Node B: " + e.getNodeB());
-            if (last.equals(e.getNodeA())) {
-                newWalk.add(e.getNodeB());
-            } else if (last.equals(e.getNodeB())) {
-                newWalk.add(e.getNodeA());
-            } else {
-                LOGGER.info("WTF??");
-                return null;
-            }
-        }
-
-        //the addition of nodes resets the internal edge list so it must be rebuilt.
-        // In the case that an edge shares both of its nodes with another edge
-        // it is possible for the list to be rebuilt properly (ie. not contain
-        // the edge being added). To rectify this situation, a backup copy of the 
-        // edge list is saved before the addition, the addition performed, the 
-        // edge explicitly added to the backup edge list, and the internal
-        // edge list replaced by the modified backup
-//	 edges.add(e);
-
-
-        //walk.m_edges = edges;
-
-        return newWalk;
-    }
-
-    private static SimpleFeature buildFeatureFromGeometry(SimpleFeature feature, Geometry geom) {
+    private static SimpleFeature buildFeatureFromGeometry(SimpleFeatureType featureType, Geometry geom) {
 
         SimpleFeatureTypeBuilder stb = new SimpleFeatureTypeBuilder();
-        stb.init(feature.getFeatureType());
-        //stb.setName("connectivityFeatureType");
-        //Add the connectivity attribute
-//        stb.add("Connectivity", Double.class);
-//        SimpleFeatureType connectivityFeatureType = stb.buildFeatureType();
-        SimpleFeatureBuilder sfb = new SimpleFeatureBuilder(feature.getFeatureType());
-        //  sfb.addAll(feature.getAttributes());
+        stb.init(featureType);
+        SimpleFeatureBuilder sfb = new SimpleFeatureBuilder(featureType);
         sfb.add(geom);
 
         return sfb.buildFeature(null);
