fd1b46308216ff7fcca3de59e2b3c2be6467e660
Gus MacAulay <angus.macaulay@unimelb.edu.au>
2012-04-24 15:29:58 +1000

    Refactored OMS wrappers to use GeoJSONUtilities. Changed Connectivity OMS wrapper to output to URL.

--- walkability/src/main/java/org/mccaughey/connectivity/ConnectivityIndexOMS.java
+++ walkability/src/main/java/org/mccaughey/connectivity/ConnectivityIndexOMS.java
@@ -6,15 +6,10 @@ package org.mccaughey.connectivity;
 
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
 import oms3.annotations.*;
 import org.geotools.data.DataUtilities;
-import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
-import org.geotools.feature.FeatureCollections;
-import org.geotools.feature.FeatureIterator;
-import org.geotools.geojson.feature.FeatureJSON;
-import org.opengis.feature.simple.SimpleFeature;
+import org.mccaughey.utilities.GeoJSONUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,7 +37,7 @@ public class ConnectivityIndexOMS {
      * The resulting connectivity
      */
     @Out
-    public String results;
+    public URL results;
 
     /**
      * Processes the featureSource network and region to calculate connectivity
@@ -52,72 +47,21 @@ public class ConnectivityIndexOMS {
     @Execute
     public void run() {
         try {
-            SimpleFeatureSource networkSource = DataUtilities.source(readFeatures(network));
-            SimpleFeatureSource regionSource = DataUtilities.source(readFeatures(regions));
+            SimpleFeatureSource networkSource = DataUtilities.source(GeoJSONUtilities.readFeatures(network));
+            SimpleFeatureSource regionSource = DataUtilities.source(GeoJSONUtilities.readFeatures(regions));
 
-            ConnectivityIndexFJ cifj = new ConnectivityIndexFJ(networkSource,regionSource.getFeatures());
+            ConnectivityIndexFJ cifj = new ConnectivityIndexFJ(networkSource, regionSource.getFeatures());
             cifj.connectivity();
-            results = writeFeatures(cifj.getResults());
-            LOGGER.info(results);
+
+            File file = new File("connectivity_regions_oms.geojson");
+            GeoJSONUtilities.writeFeatures(cifj.getResults(), file);
+            // FileUtils.writeStringToFile(file, writeFeatures(buffers));
+            results = file.toURI().toURL();
+            LOGGER.info(results.toString());
 //            System.out.println(results);
-            
+
         } catch (Exception e) { //Can't do much here because of OMS?
             LOGGER.error(e.getMessage());
         }
     }
-
-    private String writeFeatures(SimpleFeatureCollection features) throws IOException {
-        FeatureJSON fjson = new FeatureJSON();
-        Writer writer = new StringWriter();
-
-        fjson.writeFeatureCollection(features, writer);
-
-        return writer.toString();
-
-    }
-
-  
-
-    private SimpleFeatureCollection readFeatures(URL url) throws IOException {
-        FeatureJSON io = new FeatureJSON();
-      
-        String json = readURL(url);
-        Reader reader = new StringReader(json);
-        
-        FeatureIterator<SimpleFeature> features = io.streamFeatureCollection(url.openConnection().getInputStream());
-        
-    //    Reader reader = GeoJSONUtil.toReader(url.openConnection().getInputStream());
-      
-        
-        io.readFeatureCollection(reader);
-        SimpleFeatureCollection collection = FeatureCollections.newCollection();
-        SimpleFeature feature;
-        
-        while (features.hasNext()) {
-            feature = (SimpleFeature) features.next();
-            collection.add(feature);
-        }
-
-        return collection;
-    }
-
-    private String readURL(URL url) throws IOException {
-
-        URLConnection uc = url.openConnection();
-
-        InputStreamReader input = new InputStreamReader(uc.getInputStream());
-        BufferedReader in = new BufferedReader(input);
-        String inputLine;
-        StringBuilder sb = new StringBuilder();
-
-        while ((inputLine = in.readLine()) != null) {
-            sb.append(inputLine);
-        }
-
-        in.close();
-        String json = sb.toString();
-       // System.out.print(json);
-        return json;
-    }
-    
 }
--- walkability/src/main/java/org/mccaughey/connectivity/NetworkBufferOMS.java
+++ walkability/src/main/java/org/mccaughey/connectivity/NetworkBufferOMS.java
@@ -18,25 +18,20 @@ package org.mccaughey.connectivity;
 
 import java.io.*;
 import java.net.URL;
-import java.net.URLConnection;
 import oms3.annotations.Description;
 import oms3.annotations.Execute;
 import oms3.annotations.In;
 import oms3.annotations.Out;
-import org.apache.commons.io.FileUtils;
 import org.geotools.data.DataUtilities;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
-import org.geotools.feature.FeatureCollections;
-import org.geotools.feature.FeatureIterator;
-import org.geotools.geojson.feature.FeatureJSON;
-import org.opengis.feature.simple.SimpleFeature;
+import org.mccaughey.utilities.GeoJSONUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * An OMS Wrapper for Network Buffer generation
+ *
  * @author amacaulay
  */
 @Description("Generates network service areas for points on a network")
@@ -53,13 +48,11 @@ public class NetworkBufferOMS {
      */
     @In
     public URL points;
-    
     /**
      * The network distance for the service areas (maximum walk distance)
      */
     @In
     public Double distance;
-    
     /**
      * The buffer size
      */
@@ -71,75 +64,26 @@ public class NetworkBufferOMS {
     @Out
     public URL regions;
 
-    
     /**
-     * Reads the input network and point datasets then uses NetworkBufferBatch to generate all the network buffers and writes out to regions URL
+     * Reads the input network and point datasets then uses NetworkBufferBatch
+     * to generate all the network buffers and writes out to regions URL
      */
     @Execute
     public void run() {
         try {
-            SimpleFeatureSource networkSource = DataUtilities.source(readFeatures(network));
-            SimpleFeatureSource pointsSource = DataUtilities.source(readFeatures(points));
 
-            NetworkBufferBatch nbb = new NetworkBufferBatch(networkSource,pointsSource.getFeatures(),distance,bufferSize);
+            SimpleFeatureSource networkSource = DataUtilities.source(GeoJSONUtilities.readFeatures(network));
+            SimpleFeatureSource pointsSource = DataUtilities.source(GeoJSONUtilities.readFeatures(points));
+
+            NetworkBufferBatch nbb = new NetworkBufferBatch(networkSource, pointsSource.getFeatures(), distance, bufferSize);
             SimpleFeatureCollection buffers = nbb.createBuffers();
             File file = new File("service_areas_oms.json");
-            FileUtils.writeStringToFile(file, writeFeatures(buffers));
+            GeoJSONUtilities.writeFeatures(buffers, file);
+            // FileUtils.writeStringToFile(file, writeFeatures(buffers));
             regions = file.toURI().toURL();
 
         } catch (Exception e) { //Can't do much here because of OMS?
             LOGGER.error(e.getMessage());
         }
-    }
-
-    private SimpleFeatureCollection readFeatures(URL url) throws IOException {
-        FeatureJSON io = new FeatureJSON();
-
-        String json = readURL(url);
-        Reader reader = new StringReader(json);
-
-        FeatureIterator<SimpleFeature> features = io.streamFeatureCollection(url.openConnection().getInputStream());
-
-        //    Reader reader = GeoJSONUtil.toReader(url.openConnection().getInputStream());
-
-
-        io.readFeatureCollection(reader);
-        SimpleFeatureCollection collection = FeatureCollections.newCollection();
-        SimpleFeature feature;
-
-        while (features.hasNext()) {
-            feature = (SimpleFeature) features.next();
-            collection.add(feature);
-        }
-
-        return collection;
-    }
-
-    private String readURL(URL url) throws IOException {
-
-        URLConnection uc = url.openConnection();
-
-        InputStreamReader input = new InputStreamReader(uc.getInputStream());
-        BufferedReader in = new BufferedReader(input);
-        String inputLine;
-        StringBuilder sb = new StringBuilder();
-
-        while ((inputLine = in.readLine()) != null) {
-            sb.append(inputLine);
-        }
-
-        in.close();
-        String json = sb.toString();
-        // System.out.print(json);
-        return json;
-    }
-
-    private String writeFeatures(SimpleFeatureCollection features) throws IOException {
-        FeatureJSON fjson = new FeatureJSON();
-        Writer writer = new StringWriter();
-
-        fjson.writeFeatureCollection(features, writer);
-
-        return writer.toString();
     }
 }
